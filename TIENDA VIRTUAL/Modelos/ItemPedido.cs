fe7b59d70813a9820a8fab6332c1d91a6791d7a9
shirley-A21 <shirleybogado943@gmail.com>
2025-06-08 16:12:12 -0300

    clase ESTATICA,cliente,lista dentro, pedido fuera

---
+++ TIENDA VIRTUAL/Modelos/ItemPedido.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL.Modelos
+{
+    internal class ItemPedido
+    {
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public ItemPedido(Producto producto, int cantidad)
+        {
+            Producto = producto;
+            Cantidad = cantidad;
+        }
+        public decimal ObtenerSubtotal()
+        {
+            return Producto.Precio * Cantidad;
+        }
+    }
+}
---
+++ TIENDA VIRTUAL/Modelos/Pedido.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL.Modelos
+{
+    internal class Pedido
+    {
+        public int Id { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<ItemPedido> Items { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal ValorTotal { get; private set; }
+        public Pedido(int id, Cliente cliente, List<ItemPedido> items,DateTime fecha)
+        {
+            Id = id;
+            Cliente = cliente;
+            Items = items;
+            fecha = fecha;
+            ValorTotal = CalcularTotal();
+
+        }
+        private decimal CalcularTotal()
+        {
+            return Items.Sum(item => item.ObtenerSubtotal());
+        }
+        public void AplicarDescuetno(IDescuentoStrategy estrategia)
+        {
+            ValorTotal = estrategia.CalcularDescuetno(this);
+        }
+
+    }
+}
