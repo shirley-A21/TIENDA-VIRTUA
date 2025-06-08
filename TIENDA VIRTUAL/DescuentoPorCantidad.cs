f00d510c358fc8e923939c7c69947f1e006097df
shirley-A21 <shirleybogado943@gmail.com>
2025-06-08 20:53:56 -0300

    colado y el % de desc.

---
+++ TIENDA VIRTUAL/DescuentoPorCantidad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL
+{
+    public class DescuentoPorCantidad : IDescuentoStrategy
+    {
+        private readonly int cantidadMinima;
+        private readonly decimal porcentaje;
+
+        public DescuentoPorCantidad(int cantidadMinima, decimal porcentaje)
+        {
+            this.cantidadMinima = cantidadMinima;
+            this.porcentaje = porcentaje;
+        }
+
+        public decimal CalcularDescuento(Pedido pedido)
+        {
+            var totalDescuento = pedido.Items
+                .Where(i => i.Cantidad >= cantidadMinima)
+                .Sum(i => i.Subtotal() * porcentaje);
+
+            return totalDescuento;
+        }
+    }
+}
