4919e48d13817671f00403f724ba8079cf30f62d
shirley-A21 <shirleybogado943@gmail.com>
2025-06-08 15:00:48 -0300

    <modelos basicos o principales >

---
+++ TIENDA VIRTUAL/Modelos/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL.Modelos
+{
+    internal class Cliente
+    {
+        public int Id { get; }
+        public string Nombre { get;  }
+        public string Email { get; }
+        public string CPF { get;  }
+        public Cliente(int id, string nombre, string email, string cpf)
+        {
+           
+            Id = id;
+            Nombre = nombre;
+            Email = email;
+            CPF = cpf;
+        }
+    }
+}
---
+++ TIENDA VIRTUAL/Modelos/Producto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL.Modelos
+{
+    internal class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Categoria { get; set; }
+        public Producto(int id, string nombre, decimal precio, string categoria)
+        {
+            if (precio <=0)
+                throw new ArgumentException("El precio debe ser mayor que cero.");
+            Id = id;
+            Nombre = nombre;
+            Precio = precio;
+            Categoria = categoria;
+        }
+       
+    }
+}
