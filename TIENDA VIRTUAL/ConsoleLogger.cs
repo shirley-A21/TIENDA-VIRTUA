65d01355a7d4af730bb764228d1ff798cdadf757
shirley-A21 <shirleybogado943@gmail.com>
2025-06-08 21:02:27 -0300

    de la mano con la iny de dep

---
+++ TIENDA VIRTUAL/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string mensaje)
+        {
+            Console.WriteLine($"[LOG] {mensaje}");
+        }
+        //implementacion de la interfaz ILogger, que imprime mensajes en la consola.
+    }   // junto com i blogger 
+}
