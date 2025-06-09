caceb693201990a8c554c05876bd8e2ca2bf6abc
shirley-A21 <shirleybogado943@gmail.com>
2025-06-08 21:13:57 -0300

    refactorar repos . separar nterfaz de implementacion para permitir a iny de dep

---
+++ TIENDA VIRTUAL/IPedidoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL
+{
+    public interface IPedidoRepository
+    {
+        void Guardar(Pedido pedido);
+        List<Pedido> ListarTodos();
+    }
+    //define contrato , que op debe estar disponible 
+    //implementa a memory
+    //mezcle el que se debe hacr con el como se hace 
+}
