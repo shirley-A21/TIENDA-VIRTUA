217de165bb5a2e8e2b639315d7b3ea0d7c92d1c9
shirley-A21 <shirleybogado943@gmail.com>
2025-06-08 21:01:43 -0300

    coloque una iny de dep

---
+++ TIENDA VIRTUAL/Ilogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL
+{
+    public interface ILogger
+    {
+        void Log(string mensaje);
+    } //iny de depend.
+     
+}
