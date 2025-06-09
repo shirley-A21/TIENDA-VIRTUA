b82aaeff4e69baf23f232c4b2f1e353df1f5b6b0
shirley-A21 <shirleybogado943@gmail.com>
2025-06-08 21:15:20 -0300

    pedido reository ahora separado de M

---
+++ TIENDA VIRTUAL/PedidoPositoryMemory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIENDA_VIRTUAL
+{
+    public class PedidoRepositoryMemoria : IPedidoRepository
+    {
+        private readonly List<Pedido> pedidos = new List<Pedido>();
+
+        public void Guardar(Pedido pedido)
+        {
+            pedidos.Add(pedido);
+        }
+
+        public List<Pedido> ListarTodos()
+        {
+            return pedidos;
+        }
+    }
+}
